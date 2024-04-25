06744ab5636161af97cd63e0aa8d50e0802eb38e
PedroVSD <88330068+PedroVSD@users.noreply.github.com>
2024-04-25 17:36:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,39 @@ hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 let sala = document.guerySelector(".salas");
 
-let sala1 = {
-    numero: 210,
-    uso: true,
+let sala_1_I_101 = {
+    numero: 101,
+    uso: false,
+    professor:null,
+    disciplina:null,
     qntCadeiras: 50,
     tipoQuadro: "branco",
 };
 
-let sala2 = {
-    numero: 210,
-    uso: true,
+let sala_1_I_102 = {
+    numero: 102,
+    uso: false,
+    professor:null,
+    disciplina:null,
     qntCadeiras: 50,
     tipoQuadro: "branco",
 };
+
+let sala_1_I_103 = {
+    numero: 103,
+    uso: false,
+    professor:null,
+    disciplina:null,
+    qntCadeiras: 50,
+    tipoQuadro: "branco",
+};
+
+let sala_1_I_104 = {
+    numero: 104,
+    uso: false,
+    professor:null,
+    disciplina:null,
+    qntCadeiras: 50,
+    tipoQuadro: "branco",
+};
+
