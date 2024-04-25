5c2750e5d36d881321f2211af210a0164b4c79ef
PedroVSD <88330068+PedroVSD@users.noreply.github.com>
2024-04-25 13:04:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+let sala = document.guerySelector(".salas");
+
+let sala1 = {
+    numero: 210,
+    uso: true,
+    qntCadeiras: 50,
+    tipoQuadro: "branco",
+};
