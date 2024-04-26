3e365a03f44dfedfd67d7d27265dfcc699799c4e
Davi-Night <daviacs@ufrrj.br>
2024-04-26 20:53:10 -0300

    Testando novos modelos
    divs trocadas por buttons

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const button = document.querySelector(".A110");
+
+button.addEventListener("click", () => { console.log(button.classList); });
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 
-let sala = document.guerySelector(".salas");
 
-let sala_1_I_101 = {
-    numero: 101,
-    uso: false,
-    professor:null,
-    disciplina:null,
-    qntCadeiras: 50,
-    tipoQuadro: "branco",
-};
-
-let sala_1_I_102 = {
-    numero: 102,
-    uso: false,
-    professor:null,
-    disciplina:null,
-    qntCadeiras: 50,
-    tipoQuadro: "branco",
-};
-
-let sala_1_I_103 = {
-    numero: 103,
-    uso: false,
-    professor:null,
-    disciplina:null,
-    qntCadeiras: 50,
-    tipoQuadro: "branco",
-};
-
-let sala_1_I_104 = {
-    numero: 104,
-    uso: false,
-    professor:null,
-    disciplina:null,
-    qntCadeiras: 50,
-    tipoQuadro: "branco",
-};
 
