f21e98f65a3d66723087a80fbe7df0f3d27ecbf1
Davi-Night <daviacs@ufrrj.br>
2024-04-26 22:43:39 -0300

    Testando novas coisa 2.0
    melhorei os botões e agora pode mudar os andares

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
-const button = document.querySelector(".A110");
+const buttons = document.querySelectorAll(".true");
+const subir = document.querySelector("#cima");
+const descer = document.querySelector("#baixo");
+const index = document.querySelector("#Andar");
+const Primeiro = document.getElementById("1A");
+const Segundo = document.getElementById("2A");
+const Terceiro = document.getElementById("3A");
 
-button.addEventListener("click", () => { console.log(button.classList); });
+
+
+
+subir.addEventListener("click", () => {
+    if(index.textContent === "Andar: 1")
+    {
+        index.textContent = "Andar: 2";
+        Primeiro.style.zIndex = 0;
+        Segundo.style.zIndex = 10;
+    }
+
+    else if(index.textContent == "Andar: 2")
+    {
+        index.textContent = "Andar: 3";
+        Segundo.style.zIndex = 0;
+        Terceiro.style.zIndex = 10;
+    }
+
+    else
+        alert("Não tem mais andar!!!");
+
+});
+
+descer.addEventListener("click", () => {
+    if(index.textContent === "Andar: 3")
+    {
+        index.textContent = "Andar: 2";
+        Segundo.style.zIndex = 10;
+        Terceiro.style.zIndex = 0;
+    }
+
+    else if(index.textContent == "Andar: 2")
+    {
+        index.textContent = "Andar: 1";
+        Primeiro.style.zIndex = 10;
+        Segundo.style.zIndex = 0;
+    }
+
+    else
+        alert("Já está no terreo");
+
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
